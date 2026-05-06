97a6406e17a25a5f97dc57e22d35fc36cde3059a
bklein <begayym.tolomusheva@gmx.at>
2026-05-06 18:55:10 +0200

    Add Chroma setup script

---
+++ src/chroma_setup.py
@@ -0,0 +1,36 @@
+import chromadb
+import json
+from sentence_transformers import SentenceTransformer
+
+MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
+DATA_PATH = "data/training_data.jsonl"
+
+model = SentenceTransformer(MODEL_NAME)
+
+client = chromadb.PersistentClient(path="./chroma_db")
+collection = client.get_or_create_collection(name="ai_use_cases")
+
+
+def load_data():
+    data = []
+    with open(DATA_PATH, "r") as f:
+        for line in f:
+            data.append(json.loads(line))
+    return data
+
+
+data = load_data()
+
+texts = [item["text"] for item in data]
+embeddings = model.encode(texts).tolist()
+
+ids = [str(i) for i in range(len(data))]
+
+collection.add(
+    ids=ids,
+    documents=texts,
+    embeddings=embeddings,
+    metadatas=data,
+)
+
+print("Chroma DB created")
